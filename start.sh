08c735c23cbc50590761cbcb1ffb0a0e0bdd7de5
Mark Tyneway <tyneway12@gmail.com>
2018-03-04 14:41:44 -0700

    Start full node with txn index

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 ./node_modules/bcoin/bin/bcoin --prefix ~/.bcoin-testnet \
-    --network testnet --http-port 18332 --no-auth
+    --network testnet \
+    --http-port 18332 \
+    --no-auth \
+    --coin-cache 100 \
+    --index-tx \
+    --index-address \
+    --persistent-mempool \
+    --cache-size 64
 
